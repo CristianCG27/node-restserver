475d96906cfbe930d3615a4f8aa867187319ba9a
Cristian <ccg19273@gmail.com>
2023-11-13 21:05:48 -0600

    Primer commit

---
+++ controllers/usuarios.js
@@ -0,0 +1,53 @@
+const { response, request } = require("express");
+
+const usuariosGet = (req = request, res = response) => {
+  const {q, nombre = 'no name', apikey, page = 1, limit} = req.query;
+
+  res.json({
+    msg: "get API - contolador",
+    q, 
+    nombre,
+    apikey, 
+    page,
+    limit
+  });
+};
+
+const usuariosPost = (req, res = response) => {
+  const { nombre, edad } = req.body;
+
+  res.json({
+    msg: "post API - contolador",
+    nombre,
+    edad,
+  });
+};
+
+const usuariosPut = (req, res = response) => {
+  const { id } = req.params;
+
+  res.json({
+    msg: "put API - contolador",
+    id,
+  });
+};
+
+const usuariosPatch = (req, res = response) => {
+  res.json({
+    msg: "patch API - contolador",
+  });
+};
+
+const usuariosDelete = (req, res = response) => {
+  res.json({
+    msg: "delete API - contolador",
+  });
+};
+
+module.exports = {
+  usuariosGet,
+  usuariosPost,
+  usuariosPut,
+  usuariosPatch,
+  usuariosDelete,
+};
